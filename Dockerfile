d6af6821b28d5033f70593cf963b4bb2525526cb
Joshua Foster <joshua.foster@tessella.com>
2017-08-11 18:24:13 +0100

    Feature: Alter username, UID and GID for Spark Master/Worker

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,29 @@ LABEL maintainer "gareth.lloyd@stfc.ac.uk"
 ENV SPARK_VER 2.1.0
 ENV HADOOP_VER 2.7
 ENV SPARK_HOME /opt/spark
+ENV SPARK_USER datalab
+ENV SPARK_UID 1000
 
-RUN mkdir -p /opt
+USER root
 
+# Install Spark
 RUN wget -O /tmp/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz https://archive.apache.org/dist/spark/spark-${SPARK_VER}/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz && \
-	tar -zxvf /tmp/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz && \
-	rm -rf /tmp/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz && \
-	mv /spark-${SPARK_VER}-bin-hadoop${HADOOP_VER} ${SPARK_HOME}
+    tar -zxvf /tmp/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz && \
+    rm -rf /tmp/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz && \
+    mv /spark-${SPARK_VER}-bin-hadoop${HADOOP_VER} ${SPARK_HOME}
 
-ADD start.sh /usr/local/bin
-
+# Spark variables
 ENV PATH $PATH:/opt/spark/bin
 ENV SPARK_NO_DAEMONIZE 1
+
+# Add datalab user
+RUN useradd -m -s /bin/bash -N -u $SPARK_UID $SPARK_USER &&\
+    chown -R $SPARK_USER $SPARK_HOME
+
+# Install Tini
+RUN wget -O /tmp/tini https://github.com/krallin/tini/releases/download/v0.15.0/tini && \
+    mv /tmp/tini /usr/bin/tini && \
+    chmod +x /usr/bin/tini
 
 # Patch SparkR to fix issue -- https://issues.apache.org/jira/browse/SPARK-21093
 ADD daemon.R.patch /opt/spark/R/lib/SparkR/worker
@@ -28,4 +39,12 @@ RUN patch -b /opt/spark/R/lib/SparkR/worker/daemon.R /opt/spark/R/lib/SparkR/wor
 # # Spark worker web UI on 8081.
 EXPOSE 4040 7077 8080 8081
 
-CMD ["/usr/local/bin/start.sh"]
+WORKDIR ${SPARK_HOME}
+
+COPY ./start.sh /usr/local/bin
+COPY ./docker-entrypoint.sh /usr/local/bin
+
+ENTRYPOINT ["tini", "--"]
+CMD ["start.sh", "docker-entrypoint.sh"]
+
+USER $SPARK_USER
--- start.sh
+++ start.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
+# Modified from the jupyter base-notebook start.sh script
+# https://github.com/jupyter/docker-stacks/blob/master/base-notebook/start.sh
 
-unset SPARK_MASTER_PORT
+set -e
 
-ROLE="${SPARK_ROLE:?Must be set to MASTER or WORKER}"
+# If root user
+if [ $(id -u) == 0 ] ; then
+  # Only the username "datalab" was created in docker build, 
+  # therefore rename "datalab" to $SPARK_USER
+  usermod -d /home/$SPARK_USER -l $SPARK_USER datalab
 
-if [ ${ROLE} = "MASTER" ]; then
-  echo "MASTER Node"
-  echo "$(hostname -i) spark-master" >> /etc/hosts
+  # Change UID of SPARK_USER to SPARK_UID if it does not match.
+  if [ "$SPARK_UID" != $(id -u $SPARK_USER) ] ; then
+    echo "Set user UID to: $SPARK_UID"
+    usermod -u $SPARK_UID $SPARK_USER
 
-  /opt/spark/sbin/start-master.sh --ip spark-master --port 7077
+    # Fix permissions for home and spark directories
+    for d in "$SPARK_HOME" "/home/$SPARK_USER"; do
+      if [[ ! -z "$d" && -d "$d" ]]; then
+        echo "Set ownership to uid $SPARK_UID: $d"
+        chown -R $SPARK_UID "$d"
+      fi
+    done
+  fi
+
+  # Change GID of SPARK_USER to SPARK_GID, if given.
+  if [ "$SPARK_GID" ] ; then
+    echo "Change GID to $SPARK_GID"
+    groupmod -g $SPARK_GID -o $(id -g -n $SPARK_USER)
+  fi
+
+  # Exec spark docker-entrypoint as $SPARK_USER
+  echo "Execute the command as $SPARK_USER"
+  exec su $SPARK_USER -c "env PATH=$PATH $*"
 else
-  echo "WORKER Node"
-  /opt/spark/sbin/start-slave.sh spark://spark-master:7077
+if [[ ! -z "$SPARK_UID" && "$SPARK_UID" != "$(id -u)" ]]; then
+  echo 'Container must be run as root to set $SPARK_UID'
+fi
+if [[ ! -z "$SPARK_GID" && "$SPARK_GID" != "$(id -g)" ]]; then
+  echo 'Container must be run as root to set $SPARK_GID'
+fi
+  echo "Execute the command"
+  exec $*
 fi
